1f17c0b55a38aa01affd45682f902ae5b247598f
darciosoares <darciosoares28@gmail.com>
2023-06-04 18:55:05 -0300

    Arquivo Seeds, para povoar o db assim q inicializado a app

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_department (name) VALUES ('Tech');
+INSERT INTO tb_department (name) VALUES ('Pet');
+
+INSERT INTO tb_product(name,price, department_id) VALUES ('Mackbook Pro',4000.0,1);
+INSERT INTO tb_product(name,price, department_id) VALUES ('PC Pro',5000.0,1);
+INSERT INTO tb_product(name,price, department_id) VALUES ('Pet Pro',3000.0,2);
