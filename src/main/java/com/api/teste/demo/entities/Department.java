5e32c87dd9b084ef4276a035bb540aeefe79b86b
darciosoares <darciosoares28@gmail.com>
2023-06-04 18:29:54 -0300

    Relacionando as Entidades com a tabelas do banco, com JPA

--- src/main/java/com/api/teste/demo/entities/Department.java
+++ src/main/java/com/api/teste/demo/entities/Department.java
@@ -1,7 +1,16 @@
 package com.api.teste.demo.entities;
 
-public class Department {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="tb_department")
+public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
 	private String name;
 	
